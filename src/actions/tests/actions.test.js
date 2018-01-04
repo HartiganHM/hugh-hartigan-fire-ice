a62731f7d975934d22d0f48365965e49669e896a
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-04 10:51:14 -0700

    Complete initial actions test suite

--- src/actions/tests/actions.test.js
+++ src/actions/tests/actions.test.js
@@ -1,7 +1,14 @@
-import '../index';
+import * as actions from '../index';
+import mockHousesData from '../../data/mockHousesData';
 
 describe('actions tests', () => {
   it('setHousesDataInStore should have a type of SET_HOUSES_DATA', () => {
+    const housesData = mockHousesData;
+    const expected = {
+      type: 'SET_HOUSES_DATA',
+      housesData
+    }
 
+    expect(actions.setHousesDataInStore(housesData)).toEqual(expected);
   });
 })
