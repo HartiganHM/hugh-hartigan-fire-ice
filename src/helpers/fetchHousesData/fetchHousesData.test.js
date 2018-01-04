643f10d6ac7a3cd0a432801315fe072b31159667
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-04 11:41:20 -0700

    Build out anticipated fetchHouesData tests

---
+++ src/helpers/fetchHousesData/fetchHousesData.test.js
@@ -0,0 +1,15 @@
+import fetchHouseData from './fetchHousesData';
+
+describe('fetchHouseData tests', () => {
+  it('Should be called correctly', () => {
+
+  });
+
+  it('Should return an object', () => {
+
+  });
+
+  it('Should throw an error if the fetch fails', () => {
+
+  });
+});
