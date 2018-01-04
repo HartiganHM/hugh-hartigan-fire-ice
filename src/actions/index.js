eb79883675a34976cf698d86ea153dda316bb7d0
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-04 09:38:21 -0700

    Dispatch housesData to setHousesDataInStore

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,12 +2,12 @@ import fetchHousesData from '../helpers/fetchHousesData/fetchHousesData';
 
 export const populateHouseData = () => async dispatch => {
   const housesData = await fetchHousesData();
-  dispatch()
-}
+  dispatch(setHousesDataInStore(housesData));
+};
 
 export const setHousesDataInStore = housesData => {
   return {
     type: 'SET_HOUSES_DATA',
     housesData
-  }
-}
+  };
+};
