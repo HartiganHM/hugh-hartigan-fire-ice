bf50b00270f621d19f0839438d404b2959efaff4
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-04 15:48:56 -0700

    Update testing for new functionality

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -2,6 +2,7 @@ import Card from './Card';
 import React from 'react';
 import { shallow } from 'enzyme';
 import mockHousesData from '../../data/mockHousesData';
+import { read } from 'fs';
 
 describe('Card tests', () => {
   it('Card should match the snapshot', () => {
@@ -9,5 +10,22 @@ describe('Card tests', () => {
     const renderedCard = shallow(<Card houseData={mockHouseData} />);
 
     expect(renderedCard).toMatchSnapshot();
+  });
+
+  it('Card should not display sworn members by default', () => {
+    const mockHouseData = mockHousesData[0];
+    const renderedCard = shallow(<Card houseData={mockHouseData} />);
+    const expected = 0;
+
+    expect(renderedCard.find('.members').length).toEqual(expected);
+  });
+
+  it('Card should display a list of members on click', () => {
+    const mockHouseData = mockHousesData[0];
+    const renderedCard = shallow(<Card houseData={mockHouseData} />);
+    const expected = mockHouseData.swornMembers.length;
+
+    renderedCard.simulate('click');
+    expect(renderedCard.find('.members').length).toEqual(8);
   });
 });
