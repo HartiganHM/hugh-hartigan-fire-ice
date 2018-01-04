dd500a182d92b226f463b748034e81e59f3596de
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-04 12:17:09 -0700

    Complete housesReducer test suite

--- src/reducers/housesReducer.test.js
+++ src/reducers/housesReducer.test.js
@@ -1,13 +1,17 @@
-import housesReducer from './housesReducer';
+import housesData from './housesReducer';
 import * as actions from '../actions';
 import mockHousesData from '../data/mockHousesData';
 
 describe('housesReducer tests', () => {
   it('Should return the store by default', () => {
+    const expected = [];
 
+    expect(housesData(undefined, {})).toEqual(expected);
   });
 
   it('Should return a new store with housesData', () => {
+    const expected = mockHousesData;
 
+    expect(housesData(undefined, actions.setHousesDataInStore(mockHousesData))).toEqual(expected)
   });
 });
