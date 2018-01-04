e0dcf86817835a2e759ffddb30e69d84fb693334
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-04 11:34:47 -0700

    Mock out all anticipated tests for CardContainer

--- src/components/CardContainer/CardContainer.test.js
+++ src/components/CardContainer/CardContainer.test.js
@@ -6,8 +6,24 @@ import housesData from '../../data/mockHousesData';
 describe('CardContainer tests', () => {
   it('Should match the snapshot', () => {
     const mockHousesData = housesData;
-    const renderedCardContainer = shallow(<CardContainer housesData={mockHousesData}/>);
+    const renderedCardContainer = shallow(
+      <CardContainer housesData={mockHousesData} />
+    );
 
     expect(renderedCardContainer).toMatchSnapshot();
-  })
-})
+  });
+
+  it('Should show a loading image if housesData is empty', () => {
+
+  });
+
+  it('Should render Cards if housesData has content', () => {
+
+  });
+
+  describe('mapStateToProps tests', () => {
+    it('Should pull housesData from the store', () => {
+
+    });
+  });
+});
