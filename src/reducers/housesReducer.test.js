fa556c6596e01f6ee82d29088e9e43c3f3b37919
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-04 12:10:34 -0700

    Mock out anticipated housesReducer tests

--- src/reducers/housesReducer.test.js
+++ src/reducers/housesReducer.test.js
@@ -3,5 +3,11 @@ import * as actions from '../actions';
 import mockHousesData from '../data/mockHousesData';
 
 describe('housesReducer tests', () => {
-  
-})
+  it('Should return the store by default', () => {
+
+  });
+
+  it('Should return a new store with housesData', () => {
+
+  });
+});
