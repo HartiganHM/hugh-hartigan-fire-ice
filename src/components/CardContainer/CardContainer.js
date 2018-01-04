cfcae50f1c5f1f17bbafece49be9efe3fb70e5a1
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-04 10:02:20 -0700

    Add id to wolf and remove unneeded CardContainer.css

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import PropTypes from 'prop-types';
+import Card from '../Card/Card';
+import '../App/App.css'
 
 export class CardContainer extends Component {
   render() {
     return (
       <div className='CardContainer'>
-        <img src={require('./wolf.gif')} />
+        <img id='wolf' src={require('./wolf.gif')} />
       </div>
     )
   }
