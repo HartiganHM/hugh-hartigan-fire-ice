cdab1e695b9e15ac4828b32505d330b10f419bf1
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-04 11:29:57 -0700

    Build out CardContainer test file and initial test

---
+++ src/components/CardContainer/CardContainer.test.js
@@ -0,0 +1,9 @@
+import CardContainer from './CardContainer';
+import React from 'react';
+import { shallow } from 'enzyme';
+
+describe('CardContainer tests', () => {
+  it('Should match the snapshot', () => {
+    
+  })
+})
