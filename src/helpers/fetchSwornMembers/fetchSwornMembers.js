eaf2c9e331295fd3bc5d510aa249d0660d5a6792
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-04 14:18:30 -0700

    Delete function and start from scractch

--- src/helpers/fetchSwornMembers/fetchSwornMembers.js
+++ src/helpers/fetchSwornMembers/fetchSwornMembers.js
@@ -1,44 +1,9 @@
-const fetchSwornMembers = async arrayOfMembers => {
-    debugger
-    const fetchMembers = arrayOfMembers.map(async member => {
-      let postBody = { url: member }
-      console.log(postBody)
-      const fetchPerson = await fetch('http://localhost:3001/api/v1/character', {
-        method: 'POST',
-        body: JSON.stringify(postBody),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-      console.log(fetchPerson)
-      const jsonPerson = await fetchPerson.json();
-
-      return jsonPerson
+const fetchSwornMembers = async (membersArray) => {
+  try{
+    const fetchedMembers = await membersArray.map( async (member) => {
+      console.log(member)
     })
-    const jsonMemebers = await fetchMembers.json();
-
-    return Promise.all(jsonMemebers);
+  } catch (error) {
+    return Error('fuck')
+  }
 }
-
-export default fetchSwornMembers;
-
-// try {
-//   const fetchedMemebers = await swornMembers.map(async member => {
-//     const fetchMember = await fetch('http://localhost:3001/api/v1/character', {
-//       method: 'POST',
-//       body: {
-//         url: JSON.stringify(member)
-//       },
-//       headers: {
-//         'Content-Type': 'application/json'
-//       }
-//     });
-//     const jsonMember = await fetchMember.json();
-
-//     return jsonMember;
-//   });
-
-//   return fetchedMemebers;
-// } catch (error) {
-//   return Error('error retrieving sworn members');
-// }
