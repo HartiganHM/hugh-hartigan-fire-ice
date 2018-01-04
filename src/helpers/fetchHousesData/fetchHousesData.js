20a562ce4248d6105f0483e064eb7b5259096201
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-04 11:52:17 -0700

    Fix spacing

--- src/helpers/fetchHousesData/fetchHousesData.js
+++ src/helpers/fetchHousesData/fetchHousesData.js
@@ -2,10 +2,10 @@ const fetchHouseData = async () => {
   try {
     const fetchedData = await fetch('http://localhost:3001/api/v1/houses');
     const jsonData = await fetchedData.json();
-  
+
     return jsonData;
-  } catch(error) {
-    return new Error('error fetching houses data')
+  } catch (error) {
+    return new Error('error fetching houses data');
   }
 };
 
