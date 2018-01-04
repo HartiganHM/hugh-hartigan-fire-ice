a91658baecfa92a28d0b85173666bca704c8f7e2
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-04 11:43:43 -0700

    Import data to fetchHousesData and build mock window.fetch

--- src/helpers/fetchHousesData/fetchHousesData.test.js
+++ src/helpers/fetchHousesData/fetchHousesData.test.js
@@ -1,15 +1,17 @@
 import fetchHouseData from './fetchHousesData';
+import mockHousesData from '../../data/mockHousesData';
+
+window.fetch = jest.fn().mockImplementation(() =>
+  Promise.resolve({
+    status: 200,
+    json: () => Promise.resolve(mockHousesData)
+  })
+);
 
 describe('fetchHouseData tests', () => {
-  it('Should be called correctly', () => {
+  it('Should be called correctly', () => {});
 
-  });
+  it('Should return an object', () => {});
 
-  it('Should return an object', () => {
-
-  });
-
-  it('Should throw an error if the fetch fails', () => {
-
-  });
+  it('Should throw an error if the fetch fails', () => {});
 });
