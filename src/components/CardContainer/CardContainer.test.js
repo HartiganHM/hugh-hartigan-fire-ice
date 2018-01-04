60db96b9b4101c0506b15a869840c9513ba4b33e
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-04 11:32:53 -0700

    Import mockHousesData and pass initial CardContainer test

--- src/components/CardContainer/CardContainer.test.js
+++ src/components/CardContainer/CardContainer.test.js
@@ -1,9 +1,13 @@
-import CardContainer from './CardContainer';
+import { CardContainer } from './CardContainer';
 import React from 'react';
 import { shallow } from 'enzyme';
+import housesData from '../../data/mockHousesData';
 
 describe('CardContainer tests', () => {
   it('Should match the snapshot', () => {
-    
+    const mockHousesData = housesData;
+    const renderedCardContainer = shallow(<CardContainer housesData={mockHousesData}/>);
+
+    expect(renderedCardContainer).toBeDefined
   })
 })
