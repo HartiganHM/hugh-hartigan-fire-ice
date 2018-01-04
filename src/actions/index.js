a29d12d676709c8be4497f8b8041169aad008173
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-04 09:37:29 -0700

    Import fetchHousesData in actions, mock populateHousesData and setHousesDataInStore actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,13 @@
-export const fakeAction = () => ({type: 'FAKE'});
+import fetchHousesData from '../helpers/fetchHousesData/fetchHousesData';
+
+export const populateHouseData = () => async dispatch => {
+  const housesData = await fetchHousesData();
+  dispatch()
+}
+
+export const setHousesDataInStore = housesData => {
+  return {
+    type: 'SET_HOUSES_DATA',
+    housesData
+  }
+}
