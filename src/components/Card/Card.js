67a4f81751d3cbcbfc9212dbda233e4647572008
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-04 10:36:20 -0700

    Render all data points to card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,7 +4,9 @@ import PropTypes from 'prop-types';
 class Card extends Component {
   render () {
     const { name, founded, seats, titles, coatOfArms, ancestralWeapons, words } = this.props.houseData;
-    const listOfTitles = titles.map(title => <div>Title: {title}</div>)
+    const listOfTitles = titles.map(title => <div>Title: {title}</div>);
+    const listOfWeapons = ancestralWeapons.map(weapon => <div>Weapon: {weapon}</div>);
+    const listOfSeats = seats.map(seat => <div>Seat: {seat}</div>)
 
     return (
       <div className='Card'>
@@ -12,7 +14,9 @@ class Card extends Component {
         <div>Founded: {founded || 'N/A'}</div>
         <div>Words {words || 'N/A'}</div>
         {listOfTitles}
+        {listOfSeats}
         <div>Coat of Arms: {coatOfArms}</div>
+        {listOfWeapons}
       </div>
     )
   }
