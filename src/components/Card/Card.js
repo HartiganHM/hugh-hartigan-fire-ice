bf7aba263510b890c013b123cdca3561a3177d9b
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-04 10:26:04 -0700

    Render founded and listOfTitles to each Card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,10 +4,13 @@ import PropTypes from 'prop-types';
 class Card extends Component {
   render () {
     const { name, founded, seats, titles, coatOfArms, ancestralWeapons, words } = this.props.houseData;
+    const listOfTitles = titles.map(title => <div>Title: {title}</div>)
 
     return (
       <div className='Card'>
-        {name}
+        <h2>{name}</h2>
+        <span>Founded: {founded || 'N/A'}</span>
+        {listOfTitles}
       </div>
     )
   }
